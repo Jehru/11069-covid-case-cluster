5ed28f7bc510276a46628790e2f6d66b84929fbc
Jehru <jehru.e.harris@gmail.com>
2021-08-22 11:08:57 +1000

    Upload

---
+++ script.js
@@ -0,0 +1,82 @@
+//  http://bl.ocks.org/jose187/4733747
+
+console.log("Starting");
+
+var width = 960,
+    height = 700
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var force = d3.layout.force()
+    .gravity(.05)
+    .distance(100)
+    .charge(-100)
+    .size([width, height]);
+
+
+  // Creates a tooltip for all the charts except for the bubble chart
+var tooltip = d3
+  .select("body")
+  .append("div")
+  .attr("class", "tooltip")
+  .text("If you can see this, there may be something wrong");
+
+d3.json("cases.json", function(json) {
+  force
+      .nodes(json.nodes)
+      .links(json.links)
+      .start();
+
+  var link = svg.selectAll(".link")
+      .data(json.links)
+    .enter().append("line")
+      .attr("class", "link")
+    .style("stroke-width", function(d) { 
+      return Math.sqrt(d.weight); 
+    });
+
+  var node = svg.selectAll(".node")
+      .data(json.nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .call(force.drag);
+
+  node.append("circle")
+      .attr("r","7")
+      .data(json.nodes)
+      .on("mouseover", function (d) {
+        
+        //If the investigation exists
+        if(d.underInvestigation) {
+          tooltip.text("age: " + d.age + " sex: " + d.sex + " This case is being investigated");
+        } else {
+          // otherwise dont show it
+          tooltip.text("age: " + d.age + " sex: " + d.sex);
+        }
+        return tooltip.style("visibility", "visible");
+       })
+       .on("mousemove", function () {
+        return tooltip
+        .style("top", event.pageY - 5 + "px")
+        .style("left", event.pageX + 15 + "px");
+       })
+       .on("mouseout", function () {
+        return tooltip.style("visibility", "hidden");
+      });
+
+  node.append("text")
+      .attr("dx", 12)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.id});
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
+});
